bb355329e5bf96e59175dab6e41df6fa35a70349
JuanJoTm00 <juan.taborda54@correo.tdea.edu.co>
2025-03-14 19:11:44 -0500

    Create userService.js

---
+++ src/userService.js
@@ -0,0 +1,11 @@
+const users = []
+
+const addUser = (name) => {
+    const user = { id: users.length +1, name }
+    users.push(user)
+    return user
+}
+
+const getUsers = () => users
+
+module.exports = {addUser, getUsers}
