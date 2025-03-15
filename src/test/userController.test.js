dc905f7971f9900af2b9d4c6d37fba11cad9bcb0
JuanJoTm00 <juan.taborda54@correo.tdea.edu.co>
2025-03-14 19:29:49 -0500

    Create userController.test.js

---
+++ src/test/userController.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest")
+const app = require("../server")
+const userService = require("../userService")
+
+test ("Debe obtener los usuarios(integración)", async () => {
+    userService.addUser("Pepito Perez")
+    const res = await request(app).get("api/users")
+
+    expect(res.status).toBe(200)
+    expect(res.body.length).toBe(1)
+    expect(res.body[0].name).toBe("Pepito Perez ")
+})
